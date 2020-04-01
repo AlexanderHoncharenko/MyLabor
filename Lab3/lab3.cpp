95ca22ac28049ad0fb5c6222bd198cbf15ceb41a
HoncharenkoOlexander <s.viper@ukr.net>
2020-04-01 17:43:34 +0300

    redacted edition

--- Lab3/lab3.cpp
+++ Lab3/lab3.cpp
@@ -5,18 +5,20 @@ using namespace std;
 
 int main()
 {
-	const int n = 10;
+	cout << "Calculate approximately the value of the infinite sum:" << endl;
+	cout << "S = 1 + ... + [(-1)^k]*[n/(k+1)*(k+2)*(k+3)] + ... " << endl << "until absolute value of term less than epsilon" << endl;
+	cout << "Input value for n = ";
+	int n;
+	cin >> n;
+	cout << "Value for e(epsilon, accuracy) is constant = 0.0005" << endl;
 	const double e = 0.0005;
-	long double dblSum = 1;
+	double dblSum = 1;
+	double dblResult = 1;
 	int k = 1;
-	while(1)
+	while (abs(dblResult > e))
 	{
-		long double dblSumK = (k+1)*(k+2)*(k+3);
-		long double dblResult = pow(n / dblSumK, k*2);
-		if (abs(dblResult < e)) 
-		{
-			break;
-		}
+		double dblSumK = (k + 1)*(k + 2)*(k + 3);
+		dblResult = pow(n / dblSumK, k * 2);
 		if (k % 2 != 0)
 		{
 			dblResult = -1 * dblResult;
@@ -24,7 +26,7 @@ int main()
 		dblSum += dblResult;
 		k++;
 	} 
-	cout << "The result = " << fixed << dblSum <<  endl;
+	cout << "The result S = " << fixed << dblSum <<  endl;
 	system("pause");
 	return 0;
 }
--- Lab4/Lab4.cpp
+++ Lab4/Lab4.cpp
@@ -3,18 +3,20 @@
 using namespace std;
 int main() 
 {
-	cout << "Input amount of elements in array = ";
+	cout << "Make array A from array B such:" << endl << "a[i] = 1 / ( b[i] - b[i-1] ) and a[1] = 1 / ( b[1] - b[n] )" << endl;
+	cout << "Input amount of elements (i) in array B = ";
 	int n;
 	cin >> n;
 	double *B, *A;
 	B = new double[n];
+	cout << "Do not enter the same numbers in two adjacent elements" << endl;
 	for (int i = 0; i < n; i++) 
 	{
-		cout << "Input element " << i + 1 << " of array = ";
+		cout << "Input element " << i + 1 << " of array B = ";
 		cin >> B[i];
 	}
 	A = new double[n];
-	cout << "The result array is ";
+	cout << "The result array A have this numbers: " << endl;
 	for (int i = 0; i < n; i++)
 	{
 		if (i == 0)
